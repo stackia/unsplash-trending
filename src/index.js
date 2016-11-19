da58d4575ce65f70b87c1881e55acec526db5486
stackia <jsq2627@gmail.com>
2016-11-20 04:18:16 +0800

    Add deploy config for gh-pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { Provider } from 'react-redux'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 import rootReducer from './reducers/root'
 import Layout from './components/Layout'
 import PhotoList from './components/PhotoList'
@@ -13,7 +13,7 @@ const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
 render(
   <Provider store={store}>
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
       <Route path="/" component={Layout}>
         <IndexRoute component={PhotoList} />
         <Route path="photos/:page" component={PhotoList} />
